a85c335a56f20131ce14d556ba79199bdfc474b9
Kairat Ubukulov <ubukulov@gmail.com>
2022-07-25 17:37:12 +0600

    fix

---
+++ app/Classes/Halyk.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Classes;
+
+use GuzzleHttp\Client;
+class Halyk
+{
+    protected $clientId     = env('HALYK_CLIENT_ID');
+    protected $clientSecret = env('HALYK_CLIENT_SECRET');
+
+
+}
