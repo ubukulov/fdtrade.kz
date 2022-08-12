966d43de5ecb37813bc2e59cfe34a6834bedaae9
Kairat Ubukulov <ubukulov@gmail.com>
2022-08-12 16:58:44 +0600

    fx

--- app/Console/Commands/GenerateXalykXml.php
+++ app/Console/Commands/GenerateXalykXml.php
@@ -67,10 +67,7 @@ class GenerateXalykXml extends Command
                 $category = $product->category;
                 if($category) {
                     $price = round($product->price2 + ($product->price2 * ($category->margin_halyk / 100)));
-                    $name = str_replace('quot;', '', $product->name);
-                    $name = str_replace('&', '', $name);
-                    $name = str_replace('#', ' ', $name);
-                    $name = str_replace('%', ' ', $name);
+                    $name = $product->name;
                     $brand = $product->brand;
                     $this->contents .= '<offer sku="'.$product->article.'">';
                     $this->contents .= '<model>'.$name.'</model>';
