a7ce8136f69bfe5711eb968878aa35534bd8c1a1
Kairat Ubukulov <ubukulov@gmail.com>
2022-08-23 20:43:17 +0600

    fx

--- app/Console/Commands/Ozon/SyncProducts.php
+++ app/Console/Commands/Ozon/SyncProducts.php
@@ -64,7 +64,7 @@ class SyncProducts extends Command
                             'price' => (int) $price,
                             'category_id' => $oz_category->oz_category_id,
                             'offer_id' => $product->article,
-                            'vat' => 'Не облагается',
+                            'vat' => 0,
                             'weight' => 100,
                             'depth' => 10,
                             'width' => 150,
