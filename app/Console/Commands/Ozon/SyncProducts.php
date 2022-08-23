430066055a5eced09e149374f62e818ea02ae505
Kairat Ubukulov <ubukulov@gmail.com>
2022-08-23 20:42:24 +0600

    fx

--- app/Console/Commands/Ozon/SyncProducts.php
+++ app/Console/Commands/Ozon/SyncProducts.php
@@ -61,8 +61,14 @@ class SyncProducts extends Command
                         $price = $product->convertPrice('RUB', $price);
                         $arr = [
                             'name' => $product->name,
-                            'price' => $price,
+                            'price' => (int) $price,
                             'category_id' => $oz_category->oz_category_id,
+                            'offer_id' => $product->article,
+                            'vat' => 'Не облагается',
+                            'weight' => 100,
+                            'depth' => 10,
+                            'width' => 150,
+                            'height' => 250,
                         ];
 
                         // Картинки
