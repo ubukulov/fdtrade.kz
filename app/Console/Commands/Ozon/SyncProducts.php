fd726af96a694e031e5da448964c24af744b9eb8
Kairat Ubukulov <ubukulov@gmail.com>
2022-09-12 12:02:52 +0600

    fx

--- app/Console/Commands/Ozon/SyncProducts.php
+++ app/Console/Commands/Ozon/SyncProducts.php
@@ -52,105 +52,103 @@ class SyncProducts extends Command
                     ->get();
                 $oz_category = OZONCategory::findOrFail($al_oz_category->oz_category_id);*/
                 $oz_category = OZONCategory::findOrFail(9828);
+                $products = Product::where(['category_id' => 2])->get();
 
-                //if(count($products) > 0) {
-                    //foreach($products as $product) {
-                        $product = Product::find(802);
-                        $category = $product->category;
-                        $price = $product->price2 + ($product->price2 * ($category->margin_ozon / 100));
-                        $price = $product->convertPrice('RUB', $price);
-                        $arr = [
-                            'name' => $product->name,
-                            'price' => "$price",
-                            'category_id' => $oz_category->oz_category_id,
-                            //'offer_id' => "$product->article",
-                            'vat' => "Не облагается",
-                            'weight' => 100,
-                            'depth' => 10,
-                            'width' => 150,
-                            'height' => 250,
-                            'primary_image' => $product->getImage()
-                        ];
+                foreach($products as $product) {
+                    $category = $product->category;
+                    $price = $product->price2 + ($product->price2 * ($category->margin_ozon / 100));
+                    $price = $product->convertPrice('RUB', $price);
+                    $arr = [
+                        'name' => $product->name,
+                        'price' => "$price",
+                        'category_id' => $oz_category->oz_category_id,
+                        'offer_id' => "$product->article",
+                        'vat' => "Не облагается",
+                        'weight' => 100,
+                        'depth' => 10,
+                        'width' => 150,
+                        'height' => 250,
+                        'primary_image' => $product->getImage()
+                    ];
 
-                        // Картинки
-                        $images = $product->images;
-                        if(count($images) <= 1) {
-                            //continue;
-                        } else {
-                            foreach($images as $image) {
-                                $arr['images'][] = $image->path;
-                            }
+                    // Картинки
+                    $images = $product->images;
+                    if(count($images) <= 1) {
+                        //continue;
+                    } else {
+                        foreach($images as $image) {
+                            $arr['images'][] = $image->path;
                         }
+                    }
 
-                        $arr['images360'][] = $product->getThumb();
+                    $arr['images360'][] = $product->getThumb();
 
-                        $attributes = OZON::getCategoryAttributes($oz_category->oz_category_id);
-                        if($attributes) {
-                            $attributes = json_decode($attributes);
-                            foreach($attributes->result[0]->attributes as $attribute) {
-                                if($attribute->is_required) {
-                                    $att = [
-                                        'complex_id' => 0,
-                                        'id' => $attribute->id,
+                    $attributes = OZON::getCategoryAttributes($oz_category->oz_category_id);
+                    if($attributes) {
+                        $attributes = json_decode($attributes);
+                        foreach($attributes->result[0]->attributes as $attribute) {
+                            if($attribute->is_required) {
+                                $att = [
+                                    'complex_id' => 0,
+                                    'id' => $attribute->id,
+                                ];
+
+                                if($attribute->id == 85) {
+                                    $brand = (strtolower($product->brand) == 'no name') ? 'Нет бренда' : trim($product->brand);
+                                    $att['values'][] = [
+                                        'value' => $brand
                                     ];
-
-                                    if($attribute->id == 85) {
-                                        $brand = (strtolower($product->brand) == 'no name') ? 'Нет бренда' : trim($product->brand);
-                                        $att['values'][] = [
-                                            'value' => $brand
-                                        ];
-                                    }
-
-                                    if($attribute->id == 8229 || $attribute->id == 9461) {
-                                        $att['values'][] = [
-                                            'value' => 'Смартфон'
-                                        ];
-                                    }
-
-                                    if($attribute->id == 9048) {
-                                        $att['values'][] = [
-                                            'value' => $product->name
-                                        ];
-                                    }
-
-                                    if($attribute->id == 4381) {
-                                        $att['values'][] = [
-                                            'value' => "$product->article"
-                                        ];
-                                    }
-
-                                    if($attribute->id == 4385) {
-                                        $att['values'][] = [
-                                            'value' => "12"
-                                        ];
-                                    }
-
-                                    $arr['attributes'][] = $att;
                                 }
+
+                                if($attribute->id == 8229 || $attribute->id == 9461) {
+                                    $att['values'][] = [
+                                        'value' => 'Смартфон'
+                                    ];
+                                }
+
+                                if($attribute->id == 9048) {
+                                    $att['values'][] = [
+                                        'value' => $product->name
+                                    ];
+                                }
+
+                                if($attribute->id == 4381) {
+                                    $att['values'][] = [
+                                        'value' => "$product->article"
+                                    ];
+                                }
+
+                                if($attribute->id == 4385) {
+                                    $att['values'][] = [
+                                        'value' => "12"
+                                    ];
+                                }
+
+                                $arr['attributes'][] = $att;
                             }
                         }
+                    }
 
-                        $data['items'][] = $arr;
+                    $data['items'][] = $arr;
 
-                        //dd($data);
+                    //dd($data);
 
-                        //dd(json_encode($data, JSON_UNESCAPED_UNICODE));
+                    //dd(json_encode($data, JSON_UNESCAPED_UNICODE));
 
-                        $response = OZON::createOrUpdate($data);
+                    $response = OZON::createOrUpdate($data);
 
-                        if(!$response) {
-                            $this->info("Product {$product->article} don't created.");
-                            //continue;
-                        }
+                    if(!$response) {
+                        $this->info("Product {$product->article} don't created.");
+                        //continue;
+                    }
 
-                        $response = json_decode($response);
-                        if(isset($response->result)) {
-                            $this->info("The product with $product->article successfully added.");
-                        } else {
-                            $this->info("The product with $product->article failed.");
-                        }
-                    //}
-                //}
+                    $response = json_decode($response);
+                    if(isset($response->result)) {
+                        $this->info("The product with $product->article successfully added.");
+                    } else {
+                        $this->info("The product with $product->article failed.");
+                    }
+                }
            // }
 
             $this->info('The process "ozon:sync-products" is finished.');
