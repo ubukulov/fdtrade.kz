7519bb38ef5eb2a37639111b08304ddcd5c40b93
Kairat Ubukulov <ubukulov@gmail.com>
2022-10-06 14:45:45 +0600

    fx

--- app/Classes/WB.php
+++ app/Classes/WB.php
@@ -508,6 +508,7 @@ class WB
     {
         $properties = [];
         $product_feature = Style::getProductFeature($product->article);
+        dd($product_feature);
         if(isset($product_feature[0])) {
             $product_feature = $product_feature[0];
             $arr = (array) $product_feature->properties;
