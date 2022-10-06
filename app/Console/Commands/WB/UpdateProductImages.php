9d4a38684440613a3a4510882ac5cc2c045da336
Kairat Ubukulov <ubukulov@gmail.com>
2022-10-06 17:00:48 +0600

    fx

--- app/Console/Commands/WB/UpdateProductImages.php
+++ app/Console/Commands/WB/UpdateProductImages.php
@@ -39,42 +39,48 @@ class UpdateProductImages extends Command
      */
     public function handle()
     {
-        $lists = WB::getProductCardList(1000, 1000);
-        foreach($lists->data->cards as $card) {
-            if(empty($card->mediaFiles)) {
-                $product = Product::where(['wb_imtId' => $card->vendorCode])->first();
-                if($product) {
-                    $product_images = [];
-                    $images = $product->images;
+        for($i=1000; $i<=8000; $i = $i + 1000) {
+            $limit = 1000;
+            $offset = ($i == 1000) ? 0 : 1000;
+            $lists = WB::getProductCardList($limit, $offset);
+            foreach($lists->data->cards as $card) {
+                if(empty($card->mediaFiles)) {
+                    $product = Product::where(['wb_imtId' => $card->vendorCode])->first();
+                    if($product) {
+                        $product_images = [];
+                        $images = $product->images;
 
-                    if($images) {
-                        foreach($images as $image) {
-                            if($image->thumbs == 0) {
-                                $product_images[]['value'] = $image->path;
+                        if($images) {
+                            foreach($images as $image) {
+                                if($image->thumbs == 0) {
+                                    $product_images[]['value'] = $image->path;
+                                }
                             }
                         }
-                    }
 
-                    $arrImages = [];
-                    if(count($product_images) > 1) {
-                        foreach($product_images as $key=>$arr) {
-                            if($key == 0) {
-                                continue;
+                        $arrImages = [];
+                        if(count($product_images) > 1) {
+                            foreach($product_images as $key=>$arr) {
+                                if($key == 0) {
+                                    continue;
+                                }
+
+                                $arrImages[] = $arr['value'];
                             }
-
-                            $arrImages[] = $arr['value'];
                         }
-                    }
 
-                    if(count($arrImages) > 0) {
-                        $response = WB::updateProductImages($product->wb_imtId, $arrImages);
-                        if($response){
-                            $this->info("Product ".$card->vendorCode." images updated.");
+                        if(count($arrImages) > 0) {
+                            $response = WB::updateProductImages($product->wb_imtId, $arrImages);
+                            if($response){
+                                $this->info("Product ".$card->vendorCode." images updated.");
+                            }
                         }
                     }
                 }
             }
+            $this->info("----------------------------------------------------");
         }
+
 
         $this->info("Product Images process is finished.");
     }
